a84c63a48f87e3d3a1c9fad076282ad8de889f06
Firandita Yunia Rahmawati <firanditarahmawati@gmail.com>
2025-11-24 12:11:48 +0700

    perbaikan tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,19 +5,7 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#FF6600',
-        surface: '#121212',
-        brand: '#E6427B',
-        secondary: '#0f2d52',
-        info: '#00b0ad',
-        soft: '#ec9bac',
-      },
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
